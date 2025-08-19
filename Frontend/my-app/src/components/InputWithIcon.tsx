9d0e81a6c9fb995e98a2aca150f3f40a9689bf82
Kooming <eekrud3805@gmail.com>
2025-08-19 21:42:11 +0900

    20250819

--- Frontend/my-app/src/components/InputWithIcon.tsx
+++ Frontend/my-app/src/components/InputWithIcon.tsx
@@ -24,16 +24,14 @@ const InputWithIcon: React.FC<InputWithIconProps> = ({
       <label htmlFor={id} className="block text-sm font-medium text-gray-700">
         {label}
       </label>
-      {/* 👈 부모 div에 relative 클래스가 있어야 합니다. */}
       <div className="mt-1 relative rounded-md shadow-sm">
-        {/* 👈 아이콘 div에 absolute 클래스가 있어야 합니다. */}
-        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-          <FontAwesomeIcon icon={icon} className="h-5 w-5 text-gray-400" />
+        <div className="absolute inset-y-0 left-0  flex items-center pointer-events-none">
+          <FontAwesomeIcon icon={icon} className="h-5 w-5 p text-gray-400" />
         </div>
         <input
           type={type}
           id={id}
-          className="block w-full rounded-md border-gray-300 pl-10 pr-3 py-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          className="block pl-5 w-70"
           placeholder={placeholder}
         />
       </div>
--- Frontend/my-app/src/components/PasswordInput.tsx
+++ Frontend/my-app/src/components/PasswordInput.tsx
@@ -19,14 +19,14 @@ const PasswordInput = ({ id, label, placeholder }) => {
       </label>
       <div className="mt-1 relative rounded-md shadow-sm">
         {/* 왼쪽에 자물쇠 아이콘 */}
-        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+        <div className="absolute inset-y-0 left-0  flex items-center pointer-events-none">
           <FontAwesomeIcon icon={faLock} className="h-5 w-5 text-gray-400" />
         </div>
         <div>
           <input
             type={showPassword ? "text" : "password"}
             id={id}
-            className="relative"
+            className="relative pl-5 w-70"
             placeholder={placeholder}
           />
           {/* 오른쪽에 눈 아이콘 (클릭 가능) */}
